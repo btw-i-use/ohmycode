12960a5baf05467b45ab4fca04b5faff65dea4a8
navigaid <navigaid@gmail.com>
2022-04-16 16:18:41 +0800

    use vscode extensionsGallery
    https://stackoverflow.com/questions/64463768/cant-find-certain-extensions-in-code-ossopen-source-variant-of-visual-studio-c
    
    https://aur.archlinux.org/cgit/aur.git/tree/patch.sh?h=code-marketplace

--- src/vs/code/browser/workbench/workbench.ts
+++ src/vs/code/browser/workbench/workbench.ts
@@ -494,6 +494,8 @@ function doCreateUri(path: string, queryValues: Map<string, string>): URI {
 	}
 	const config: IWorkbenchConstructionOptions & { folderUri?: UriComponents; workspaceUri?: UriComponents } = JSON.parse(configElementAttribute);
 
+	console.log('vscode-workbench-web-configuration', JSON.stringify(config, null, '  '))
+
 	// Create workbench
 	create(document.body, {
 		...config,
